83aae6be5d7eb7f6eade07360ef0a0438b20e3c4
namgeon1106 <namgeon1106@naver.com>
2022-08-26 16:42:15 +0900

    Initial Commit

---
+++ ThreeLinesSummary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreeLinesSummary
+//
+//  Created by 김남건 on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
