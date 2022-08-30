f874a5704dd086d2b1acc70acab0009eaf32fe20
namgeon1106 <namgeon1106@naver.com>
2022-08-31 00:42:04 +0900

    feat: 번역 API 호출 결과 decoding할 TranslateDataDecoder 추가

---
+++ ThreeLinesSummary/Models/TranslateDataDecoder.swift
@@ -0,0 +1,20 @@
+//
+//  TranslateDataDecoder.swift
+//  ThreeLinesSummary
+//
+//  Created by 김남건 on 2022/08/31.
+//
+
+import Foundation
+
+struct TranslateDataDecoder: Decodable {
+    let message: Message
+    
+    struct Message: Decodable {
+        let result: Result
+        
+        struct Result: Decodable {
+            let translatedText: String
+        }
+    }
+}
