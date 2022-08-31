885e19fabcece43df8a0b0283a5245cc7459a38f
namgeon1106 <namgeon1106@naver.com>
2022-08-31 15:33:07 +0900

    test: NetworkManagerIntegrationTests 추가

---
+++ ThreeLinesSummaryTests/Network/NetworkManagerIntegrationTests.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkManagerIntegrationTests.swift
+//  ThreeLinesSummaryTests
+//
+//  Created by 김남건 on 2022/08/31.
+//
+
+import XCTest
+@testable import ThreeLinesSummary
+
+class NetworkManagerIntegrationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTranslate() {
+        let sut = NetworkManager(urlSession: URLSession.shared)
+        let expectation = expectation(description: "Task should be executed during test.")
+        
+        Task {
+            do {
+                let text = try await sut.translate("I believe I can fly.")
+                XCTAssertEqual(text, "난 내가 날 수 있다고 믿어요.")
+                expectation.fulfill()
+            } catch {
+                
+            }
+            
+        }
+        
+        wait(for: [expectation], timeout: 3)
+    }
+
+}
