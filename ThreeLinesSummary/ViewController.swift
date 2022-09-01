71dafebc704d758df197852165ef386f1975805c
namgeon1106 <namgeon1106@naver.com>
2022-09-01 13:35:29 +0900

    feat: 각 view를 ViewController의 프로퍼티로 추가

--- ThreeLinesSummary/ViewController.swift
+++ ThreeLinesSummary/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let pasteView = PasteView()
+    let translateView = TranslateView()
+    let summaryView = SummaryView()
+    let loadingView = LoadingView()
+    let errorView = ErrorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
