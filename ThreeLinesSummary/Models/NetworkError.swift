05e5ed3d44fea92f93c43972f502b808b39b55c7
namgeon1106 <namgeon1106@naver.com>
2022-09-01 14:27:29 +0900

    feat: NetworkError에 message 프로퍼티 추가

--- ThreeLinesSummary/Models/NetworkError.swift
+++ ThreeLinesSummary/Models/NetworkError.swift
@@ -14,4 +14,21 @@ enum NetworkError: Error {
     case emptyText
     case encoding
     case invalidText
+    
+    var message: String {
+        switch self {
+        case .serverError:
+            return "서버에서 에러가 발생했습니다. 다시 시도해보세요."
+        case .unknown:
+            return "알 수 없는 에러가 발생했습니다."
+        case .longText:
+            return "텍스트가 너무 깁니다."
+        case .emptyText:
+            return "텍스트가 비어 있습니다."
+        case .encoding:
+            return "인코딩에 문제가 있습니다."
+        case .invalidText:
+            return "유효하지 않은 텍스트입니다."
+        }
+    }
 }
