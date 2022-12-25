894bbdddd85432f67b7a594799b88871a7d6ac64
namgeon1106 <namgeon1106@naver.com>
2022-12-25 21:36:34 +0900

    feat: SummaryViewController 추가

---
+++ ThreeLinesSummary/SummaryTab/SummaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryViewController.swift
+//  ThreeLinesSummary
+//
+//  Created by 김남건 on 2022/12/25.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+    let koreanTextView = KoreanTextView(isFirstView: true)
+    let summaryView = SummaryView()
+    let errorView = ErrorView()
+    
+    override func viewDidLoad() {
+        
+    }
+}
